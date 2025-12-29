85178998af544eef9a14d9f98d86743fba40878f
dpanshunarwal <thedeepanshunarwal@gmail.com>
2025-12-29 22:57:02 +0530

    fix: g_ptraceMem static/extern conflict - add definition in ptrace_memory.cpp

--- app/src/main/cpp/ptrace_memory.cpp
+++ app/src/main/cpp/ptrace_memory.cpp
@@ -1,132 +1,136 @@
 #include "ptrace_memory.h"
+#include <android/log.h>
+#include <errno.h>
+#include <string.h>
 #include <sys/ptrace.h>
 #include <sys/uio.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <android/log.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "PtraceMem", __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "PtraceMem", __VA_ARGS__)
+#define LOGI(...)                                                              \
+  __android_log_print(ANDROID_LOG_INFO, "PtraceMem", __VA_ARGS__)
+#define LOGE(...)                                                              \
+  __android_log_print(ANDROID_LOG_ERROR, "PtraceMem", __VA_ARGS__)
+
+// Global instance (extern in header)
+PtraceMemory *g_ptraceMem = nullptr;
 
 PtraceMemory::PtraceMemory() : m_pid(-1), m_attached(false) {}
 
-PtraceMemory::~PtraceMemory() {
-    detach();
-}
+PtraceMemory::~PtraceMemory() { detach(); }
 
 bool PtraceMemory::attach(int pid) {
-    if (m_attached) {
-        detach();
-    }
-    
-    m_pid = pid;
-    
-    // Try ptrace attach (may fail in ADB mode due to SELinux)
-    if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) == 0) {
-        waitpid(pid, nullptr, 0);
-        m_attached = true;
-        LOGI("Attached to PID %d via ptrace", pid);
-        return true;
-    }
-    
-    // If ptrace fails, we can still use process_vm_readv without attaching
-    // This works in ADB mode for reading (not writing)
-    LOGI("Ptrace attach failed (errno=%d), using process_vm_readv only", errno);
-    m_pid = pid;
-    m_attached = false; // Not attached but can still read
+  if (m_attached) {
+    detach();
+  }
+
+  m_pid = pid;
+
+  // Try ptrace attach (may fail in ADB mode due to SELinux)
+  if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) == 0) {
+    waitpid(pid, nullptr, 0);
+    m_attached = true;
+    LOGI("Attached to PID %d via ptrace", pid);
     return true;
+  }
+
+  // If ptrace fails, we can still use process_vm_readv without attaching
+  // This works in ADB mode for reading (not writing)
+  LOGI("Ptrace attach failed (errno=%d), using process_vm_readv only", errno);
+  m_pid = pid;
+  m_attached = false; // Not attached but can still read
+  return true;
 }
 
 void PtraceMemory::detach() {
-    if (m_attached && m_pid > 0) {
-        ptrace(PTRACE_DETACH, m_pid, nullptr, nullptr);
-        m_attached = false;
-    }
-    m_pid = -1;
+  if (m_attached && m_pid > 0) {
+    ptrace(PTRACE_DETACH, m_pid, nullptr, nullptr);
+    m_attached = false;
+  }
+  m_pid = -1;
 }
 
-bool PtraceMemory::readMemory(uintptr_t address, void* buffer, size_t size) {
-    if (m_pid <= 0 || !buffer || size == 0) {
-        return false;
-    }
-    
-    // Use process_vm_readv - works in ADB mode!
-    struct iovec local[1];
-    struct iovec remote[1];
-    
-    local[0].iov_base = buffer;
-    local[0].iov_len = size;
-    remote[0].iov_base = (void*)address;
-    remote[0].iov_len = size;
-    
-    ssize_t nread = process_vm_readv(m_pid, local, 1, remote, 1, 0);
-    
-    if (nread == (ssize_t)size) {
-        return true;
-    }
-    
-    if (nread < 0) {
-        // LOGE("process_vm_readv failed at 0x%lx: %s", address, strerror(errno));
-    }
-    
+bool PtraceMemory::readMemory(uintptr_t address, void *buffer, size_t size) {
+  if (m_pid <= 0 || !buffer || size == 0) {
     return false;
+  }
+
+  // Use process_vm_readv - works in ADB mode!
+  struct iovec local[1];
+  struct iovec remote[1];
+
+  local[0].iov_base = buffer;
+  local[0].iov_len = size;
+  remote[0].iov_base = (void *)address;
+  remote[0].iov_len = size;
+
+  ssize_t nread = process_vm_readv(m_pid, local, 1, remote, 1, 0);
+
+  if (nread == (ssize_t)size) {
+    return true;
+  }
+
+  if (nread < 0) {
+    // LOGE("process_vm_readv failed at 0x%lx: %s", address, strerror(errno));
+  }
+
+  return false;
 }
 
-bool PtraceMemory::writeMemory(uintptr_t address, const void* buffer, size_t size) {
-    if (m_pid <= 0 || !buffer || size == 0) {
-        return false;
-    }
-    
-    // Use process_vm_writev - may fail in ADB mode
-    struct iovec local[1];
-    struct iovec remote[1];
-    
-    local[0].iov_base = (void*)buffer;
-    local[0].iov_len = size;
-    remote[0].iov_base = (void*)address;
-    remote[0].iov_len = size;
-    
-    ssize_t nwritten = process_vm_writev(m_pid, local, 1, remote, 1, 0);
-    
-    if (nwritten == (ssize_t)size) {
-        return true;
-    }
-    
-    if (nwritten < 0) {
-        LOGE("process_vm_writev failed at 0x%lx: %s", address, strerror(errno));
-    }
-    
+bool PtraceMemory::writeMemory(uintptr_t address, const void *buffer,
+                               size_t size) {
+  if (m_pid <= 0 || !buffer || size == 0) {
     return false;
+  }
+
+  // Use process_vm_writev - may fail in ADB mode
+  struct iovec local[1];
+  struct iovec remote[1];
+
+  local[0].iov_base = (void *)buffer;
+  local[0].iov_len = size;
+  remote[0].iov_base = (void *)address;
+  remote[0].iov_len = size;
+
+  ssize_t nwritten = process_vm_writev(m_pid, local, 1, remote, 1, 0);
+
+  if (nwritten == (ssize_t)size) {
+    return true;
+  }
+
+  if (nwritten < 0) {
+    LOGE("process_vm_writev failed at 0x%lx: %s", address, strerror(errno));
+  }
+
+  return false;
 }
 
 int PtraceMemory::readInt(uintptr_t address) {
-    int value = 0;
-    readMemory(address, &value, sizeof(value));
-    return value;
+  int value = 0;
+  readMemory(address, &value, sizeof(value));
+  return value;
 }
 
 long PtraceMemory::readLong(uintptr_t address) {
-    long value = 0;
-    readMemory(address, &value, sizeof(value));
-    return value;
+  long value = 0;
+  readMemory(address, &value, sizeof(value));
+  return value;
 }
 
 float PtraceMemory::readFloat(uintptr_t address) {
-    float value = 0;
-    readMemory(address, &value, sizeof(value));
-    return value;
+  float value = 0;
+  readMemory(address, &value, sizeof(value));
+  return value;
 }
 
 bool PtraceMemory::writeInt(uintptr_t address, int value) {
-    return writeMemory(address, &value, sizeof(value));
+  return writeMemory(address, &value, sizeof(value));
 }
 
 bool PtraceMemory::writeLong(uintptr_t address, long value) {
-    return writeMemory(address, &value, sizeof(value));
+  return writeMemory(address, &value, sizeof(value));
 }
 
 bool PtraceMemory::writeFloat(uintptr_t address, float value) {
-    return writeMemory(address, &value, sizeof(value));
+  return writeMemory(address, &value, sizeof(value));
 }
--- app/src/main/cpp/remote_jni.cpp
+++ app/src/main/cpp/remote_jni.cpp
@@ -3,88 +3,90 @@
  * Uses process_vm_readv/writev for non-root memory access
  */
 
-#include <jni.h>
-#include <android/log.h>
 #include "ptrace_memory.h"
+#include <android/log.h>
+#include <jni.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "RemoteJNI", __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "RemoteJNI", __VA_ARGS__)
 
-// Global PtraceMemory instance for RemoteService
-static PtraceMemory* g_ptraceMem = nullptr;
+#define LOGI(...)                                                              \
+  __android_log_print(ANDROID_LOG_INFO, "RemoteJNI", __VA_ARGS__)
+#define LOGE(...)                                                              \
+  __android_log_print(ANDROID_LOG_ERROR, "RemoteJNI", __VA_ARGS__)
+
+// Use global from ptrace_memory.h (extern PtraceMemory* g_ptraceMem)
 
 extern "C" {
 
-JNIEXPORT jboolean JNICALL
-Java_com_gameesp_RemoteService_nativeAttach(JNIEnv* env, jobject thiz, jint pid) {
-    LOGI("nativeAttach called for PID: %d", pid);
-    
-    if (!g_ptraceMem) {
-        g_ptraceMem = new PtraceMemory();
-    }
-    
-    bool result = g_ptraceMem->attach(pid);
-    LOGI("Attach result: %s", result ? "success" : "failed");
-    return result ? JNI_TRUE : JNI_FALSE;
+JNIEXPORT jboolean JNICALL Java_com_gameesp_RemoteService_nativeAttach(
+    JNIEnv *env, jobject thiz, jint pid) {
+  LOGI("nativeAttach called for PID: %d", pid);
+
+  if (!g_ptraceMem) {
+    g_ptraceMem = new PtraceMemory();
+  }
+
+  bool result = g_ptraceMem->attach(pid);
+  LOGI("Attach result: %s", result ? "success" : "failed");
+  return result ? JNI_TRUE : JNI_FALSE;
 }
 
 JNIEXPORT void JNICALL
-Java_com_gameesp_RemoteService_nativeDetach(JNIEnv* env, jobject thiz) {
-    LOGI("nativeDetach called");
-    
-    if (g_ptraceMem) {
-        g_ptraceMem->detach();
-    }
+Java_com_gameesp_RemoteService_nativeDetach(JNIEnv *env, jobject thiz) {
+  LOGI("nativeDetach called");
+
+  if (g_ptraceMem) {
+    g_ptraceMem->detach();
+  }
 }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_gameesp_RemoteService_nativeReadBytes(JNIEnv* env, jobject thiz, jlong address, jint size) {
-    if (!g_ptraceMem || size <= 0 || size > 1024 * 1024) {
-        return nullptr;
-    }
-    
-    // Allocate temporary buffer
-    uint8_t* buffer = new uint8_t[size];
-    
-    bool success = g_ptraceMem->readMemory((uintptr_t)address, buffer, size);
-    
-    if (!success) {
-        delete[] buffer;
-        return nullptr;
-    }
-    
-    // Create Java byte array
-    jbyteArray result = env->NewByteArray(size);
-    if (result) {
-        env->SetByteArrayRegion(result, 0, size, (jbyte*)buffer);
-    }
-    
+JNIEXPORT jbyteArray JNICALL Java_com_gameesp_RemoteService_nativeReadBytes(
+    JNIEnv *env, jobject thiz, jlong address, jint size) {
+  if (!g_ptraceMem || size <= 0 || size > 1024 * 1024) {
+    return nullptr;
+  }
+
+  // Allocate temporary buffer
+  uint8_t *buffer = new uint8_t[size];
+
+  bool success = g_ptraceMem->readMemory((uintptr_t)address, buffer, size);
+
+  if (!success) {
     delete[] buffer;
-    return result;
+    return nullptr;
+  }
+
+  // Create Java byte array
+  jbyteArray result = env->NewByteArray(size);
+  if (result) {
+    env->SetByteArrayRegion(result, 0, size, (jbyte *)buffer);
+  }
+
+  delete[] buffer;
+  return result;
 }
 
-JNIEXPORT jboolean JNICALL
-Java_com_gameesp_RemoteService_nativeWriteBytes(JNIEnv* env, jobject thiz, jlong address, jbyteArray data) {
-    if (!g_ptraceMem || !data) {
-        return JNI_FALSE;
-    }
-    
-    jsize size = env->GetArrayLength(data);
-    if (size <= 0) {
-        return JNI_FALSE;
-    }
-    
-    // Get data from Java array
-    jbyte* buffer = env->GetByteArrayElements(data, nullptr);
-    if (!buffer) {
-        return JNI_FALSE;
-    }
-    
-    bool success = g_ptraceMem->writeMemory((uintptr_t)address, buffer, size);
-    
-    env->ReleaseByteArrayElements(data, buffer, JNI_ABORT);
-    
-    return success ? JNI_TRUE : JNI_FALSE;
+JNIEXPORT jboolean JNICALL Java_com_gameesp_RemoteService_nativeWriteBytes(
+    JNIEnv *env, jobject thiz, jlong address, jbyteArray data) {
+  if (!g_ptraceMem || !data) {
+    return JNI_FALSE;
+  }
+
+  jsize size = env->GetArrayLength(data);
+  if (size <= 0) {
+    return JNI_FALSE;
+  }
+
+  // Get data from Java array
+  jbyte *buffer = env->GetByteArrayElements(data, nullptr);
+  if (!buffer) {
+    return JNI_FALSE;
+  }
+
+  bool success = g_ptraceMem->writeMemory((uintptr_t)address, buffer, size);
+
+  env->ReleaseByteArrayElements(data, buffer, JNI_ABORT);
+
+  return success ? JNI_TRUE : JNI_FALSE;
 }
 
 } // extern "C"
